bb45093f9cfffa8bc18ba5e717eeb3df464c7ab2
OlehKobrynovych <o.kobrynovych@gmail.com>
2022-05-15 13:16:43 +0300

    upgrade geme

--- src/components/Main.js
+++ src/components/Main.js
@@ -17,12 +17,15 @@ function Main() {
     const dispatch = useDispatch();
     const change = useSelector(state=>state.change)
     const items = useSelector(state=>state.items)
+    const [warning, setWarning] = useState(null)
+
     // const computer = useSelector(state=>state.computer)  //++
 
     const handleClick = (id) => {
         if (change) {
             dispatch({type:'SET_ITEM_ID', payload: {id: id, user: change}})
             dispatch({type:'SET_CHANGE', payload: change })
+            setWarning(null)
         }
     }
     
@@ -47,7 +50,7 @@ function Main() {
         const value = win.some(element=>element.every(el => user.some(ell=>ell.id===el)));
         
         if (value) {
-            setWiner(`User${id} winner`);
+            setWiner(`User ${id} winner`);
         }
     }
 
@@ -56,14 +59,18 @@ function Main() {
         <>
             {!!change && (
                 <div>
-                    {!!winer && (<div className="winer" >{winer}</div>)}
-                
+                    {!!winer && (<div className="winer"><h2>{winer}</h2></div>)}
+
+                    {!winer && (<div><h2>Choice of {change} User {change===1 ? 'O' : 'X'}</h2></div>)}
+
+                    {!!warning && !winer && (<div><h2 className='warningText'>{warning}</h2></div>)}
+
                     <div className='wrapper'>
                         {
                             !!change && items.map(el=>(
                                 <div 
                                     key={el.id}
-                                    onClick={()=>!el.user && !winer ? handleClick(el.id) : null} 
+                                    onClick={()=>!el.user && !winer ? handleClick(el.id) : setWarning('Make another choice!!!')} 
                                     className={`item ${el.user === 1 ? 'item--red' : el.user === 2 ? 'item--blue' : ''}`}
                                 >
                                     {el.id}
@@ -73,7 +80,7 @@ function Main() {
                             </div>
                         <div>
                         
-                        {!!change && <button onClick={()=>restart()}>Restart</button>}
+                        {!!change && <button className='restartBtn' onClick={()=>restart()}>Restart</button>}
                     </div>
                 </div>
             )}
